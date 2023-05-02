96834295628c87c9ce21b793874c597f14bb765c
Olena Korenieva <olena.korenieva@gmail.com>
2023-05-02 17:06:25 +0200

    fixid protocol

--- src/services/apiCities.js
+++ src/services/apiCities.js
@@ -5,7 +5,7 @@ const API_KEY = "78892f44b81d56714507067e29575b82";
 
 export const getCityLocation = async (q) => {
   const { data } = await axios.get(
-    "http://api.openweathermap.org/geo/1.0/direct",
+    "https://api.openweathermap.org/geo/1.0/direct",
     {
       params: {
         q,
