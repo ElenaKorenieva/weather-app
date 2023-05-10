e991c3e69f02ed713a35989f752b4c2565286636
Olena Korenieva <olena.korenieva@gmail.com>
2023-05-10 20:36:43 +0200

    fix unused variables at the state

--- src/pages/FiveDaysWeather/FiveDaysWeather.jsx
+++ src/pages/FiveDaysWeather/FiveDaysWeather.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import PropTypes from "prop-types";
 import { getFiveDaysWeather } from "services/apiCurrentWeather";
 import WeatherItem from "components/WeatherItem/WeatherItem";
@@ -7,7 +7,7 @@ import Location from "../../components/Location/Location";
 
 const FiveDaysWeather = ({ city, location }) => {
   const [weatherFiveDays, setWeatherFiveDays] = useState([]);
-  // eslint-disable-next-line no-unused-vars;
+  // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState("");
 
   useEffect(() => {
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,8 +15,7 @@ import IconSelector from "components/IconSelector/IconSelector";
 
 const Home = ({ city, location }) => {
   const [dayWeather, setDayWeather] = useState({});
-
-  // eslint-disable-next-line no-unused-vars;
+  // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState(null);
   const [quotes, setQuotes] = useState([]);
 
